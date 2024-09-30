432e653b2da186914c13adb2cd6a22f3626b6552
pmg <gachigipm@gmail.com>
2024-09-30 02:51:22 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(spy){
+spy();
+}
+function returnsANamedFunction(){
+    function tmt(){ return 'timotheo'}
+     return tmt
+}
+function returnsAnAnonymousFunction(){
+   return function(){
+    sembe
+   }
+}
